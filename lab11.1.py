0cb8e55905f028d316245a54702c60529c23e9ae
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-12-09 11:35:07 +0300

    Update lab11.1.py

--- lab11.1.py
+++ lab11.1.py
@@ -1,26 +1,19 @@
 from math import *
-class Pyramid():
-    def Volume(self, Ploshad, Visota):
+class PyramidVolume():
+    def __init__(self, Ploshad, Visota, Kolvostr):
         self.S = Ploshad
         self.h = Visota
-        self.rezVolume = (self.S * self.h) / 3
-    def Side(self, Kolvostr, Ploshad, Visota):
         self.n = Kolvostr
-        self.S = Ploshad
-        self.h = Visota
-        self.rezBokPoverhnost = ((self.n * self.S) / 2) * (sqrt((self.h * self.h) + (self.s / (2 * tan(180 / self.n)))) )                        
-oper = int(input("Введите желаемою операцию \n 0 - Объем пирамиды \n 1 - Объем боковой поверхности пирамиды: "))
-if oper == 1 or oper == 0:
-    if oper == 0:
-        plos = int(input("Введите площадь основания пирамиды: "))
-        vis = int(input("Введите высоту от основания пирамиды: "))
-        rezul = Pyramid.Volume(0, plos, vis)
-        print("Объем пирамиды = ", rezul)
-    if oper == 1:
-        kolstr = int(input("Введите количество сторон вашей пирамиды: "))
-        plos = int(input("Введите площадь основания пирамиды: "))
-        vis = int(input("Введите высоту от основания пирамиды: "))
-        rezul = Pyramid.Side(0, kolstr, plos, vis)
-        print("Объем боковой стороны пирамиды = ", rezul)
-else:
-    print("Вы ввели неверное значение!")
+    def Volume(self):
+        self.rezVolume = (self.S * self.h) / 3
+        print("Объем пирамиды = ", self.rezVolume)
+    def Side(self):
+        self.rezBokPoverhnost = ((self.n * self.S) / 2) * (sqrt((self.h * self.h) + (self.S / (2 * tan(180 / self.n)))))
+        print("Площадь боковой поверхности пирамиды = ", self.rezBokPoverhnost)
+plos = int(input("Введите площадь основания пирамиды: "))
+vis = int(input("Введите высоту от основания пирамиды: "))
+kolstr = int(input("Введите количество сторон вашей пирамиды: "))
+vol = PyramidVolume(plos, vis, None)
+vol.Volume()
+Sid = PyramidVolume(plos, vis, kolstr)
+Sid.Side()
